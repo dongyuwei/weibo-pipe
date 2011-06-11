b3e22c474c6d1e9ab702f44e2df70b48e076decd
newdongyuwei <newdongyuwei@localhost>
2011-06-11 23:44:15 +0800

    mv to weibo.session.im

--- main.rb
+++ main.rb
@@ -7,14 +7,14 @@ Weibo::Config.api_secret = "651156f9b786fdc0781376f3b121f359"
 
 REDIS = Redis.new(:thread_safe=>true,:db=>1)
 
-get '/weibo' do
+get '/' do
   if session[:atoken]
-    redirect "/weibo/list"
+    redirect "/list"
   end
-  "<a href='/weibo/connect'>授权，分享微博订阅</a>"
+  "<a href='/connect'>授权，分享微博订阅</a>"
 end
 
-get "/weibo/list/:key" do |key|
+get "/list/:key" do |key|
       list = REDIS.get(key).split("&")
       oauth = Weibo::OAuth.new(Weibo::Config.api_key, Weibo::Config.api_secret)
       oauth.authorize_from_access(list[1], list[2])
@@ -24,41 +24,41 @@ get "/weibo/list/:key" do |key|
       Haml::Engine.new(File.read("./views/friend_timeline.haml")).render(self)
 end
     
-get "/weibo/list" do
+get "/list" do
     list = []
     list.push("<ul>")
     REDIS.keys.each do |key| 
-        list.push("<li>查看<a target='_blank' href='/weibo/list/#{key}'>#{REDIS.get(key).split("&")[0]}</a>订阅的微博</li>")
+        list.push("<li>查看<a target='_blank' href='/list/#{key}'>#{REDIS.get(key).split("&")[0]}</a>订阅的微博</li>")
     end
     list.push("</ul>")
     list.join("")
 end
 
-get '/weibo/connect' do
+get '/connect' do
   oauth = Weibo::OAuth.new(Weibo::Config.api_key, Weibo::Config.api_secret)
   request_token = oauth.consumer.get_request_token
   session[:rtoken], session[:rsecret] = request_token.token, request_token.secret
-  redirect "#{request_token.authorize_url}&oauth_callback=http://session.im/weibo/callback"
+  redirect "#{request_token.authorize_url}&oauth_callback=http://weibo.session.im/callback"
 end
 
-get '/weibo/callback' do
+get '/callback' do
   oauth = Weibo::OAuth.new(Weibo::Config.api_key, Weibo::Config.api_secret)
   oauth.authorize_from_request(session[:rtoken], session[:rsecret], params[:oauth_verifier])
   session[:rtoken], session[:rsecret] = nil, nil
   session[:atoken], session[:asecret] = oauth.access_token.token, oauth.access_token.secret
-  redirect "/weibo/verify_credentials"
+  redirect "/verify_credentials"
 end
 
-get '/weibo/verify_credentials' do
+get '/verify_credentials' do
     oauth = Weibo::OAuth.new(Weibo::Config.api_key, Weibo::Config.api_secret)
     oauth.authorize_from_access(session[:atoken], session[:asecret])
     credentials = Weibo::Base.new(oauth).verify_credentials
     puts credentials[:name] ,credentials[:id] ,credentials[:profile_image_url] ,credentials[:description]
     REDIS.set(credentials[:id], [credentials[:name],session[:atoken], session[:asecret]].join('&'))
-    redirect "/weibo/list"
+    redirect "/list"
 end
 
-get '/weibo/logout' do
+get '/logout' do
   session[:atoken], session[:asecret] = nil, nil
-  redirect "/weibo"
+  redirect "/"
 end
